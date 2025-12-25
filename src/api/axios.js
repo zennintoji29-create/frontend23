64b585a91b719f8891fd847798e395352859cfaa
subhansumain <zennintoji29@gmail.com>
2025-12-25 19:22:02 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://backend23-wuq7.onrender.com/api',
+
   headers: {
     'Content-Type': 'application/json',
   },
